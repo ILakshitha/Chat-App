ab36a67889347fc21b7789b362cd4bc82a6608e2
ILakshitha <ilakshitha7921@gmail.com>
2025-05-06 16:29:36 +0530

    complete update profile and check auth functions

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import { genarateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
@@ -94,3 +95,33 @@ export const logout = (req,res)=>{
         
     }
 };
+
+export const updateProfile = async(req,res)=>{
+  try {
+    const {profilePic} = req.body;
+    const userId = req.user._id;
+
+    if (!profilePic) {
+        return res.status(400).json({message:"Profile Pic is required"})
+    }
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(userId,{profilePic:uploadResponse.secure_url},{new:true});
+
+    res.status(200).json(updateUser);
+  } catch (error) {
+    console.log("error in update profile");
+    
+    res.status(500).json({message:"Internal server error"});
+    
+  }  
+};
+
+export const checkAuth = async(req,res)=>{
+    try {
+        res.status(200).json(req.user);
+    } catch (error) {
+        console.log("error in checkAuth Controller",error.message);;
+    
+        res.status(500).json({message:"Internal server error"});  
+    }
+}
