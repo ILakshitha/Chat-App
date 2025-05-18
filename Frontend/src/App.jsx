0700dadf240673bfea41a8a63d16b68383b0d4f4
ILakshitha <ilakshitha7921@gmail.com>
2025-05-18 12:00:15 +0530

    created nave bar and routes done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar.jsx';
+import HomePage from './pages/HomePage.jsx';
+import SignUpPage from './pages/SignUpPage.jsx';
+import LoginPage from './pages/LoginPage.jsx';
+import SettingPage from './pages/SettingPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import { useAuthStore } from './store/useAuthStore.js';
+import { Loader } from 'lucide-react';
+
 
 const App = () => {
+  const { user, checkAuth, isCheckingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+
+  }, [checkAuth])
+
+  console.log(user);
+
+  if (isCheckingAuth && !user) return (<div className='flex justify-center items-center h-screen'>
+<Loader className='animate-spin size-10' />
+  </div>);
+
   return (
 
-    <div className='text-red-400'>
+    <div >
 
-      <h1>Ishan Lakshitha</h1>
-      Hello world!
+      <Navbar />
+      <Routes>
+        <Route path="/" element={user?<HomePage />: <Navigate to="/login"/>} />
+        <Route path="/signup" element={!user?<SignUpPage />:<Navigate to="/"/>} />
 
-      <div className="badge badge-primary">Primary</div>
-<div className="badge badge-secondary">Secondary</div>
-<div className="badge badge-accent">Accent</div>
-<div className="badge badge-neutral">Neutral</div>
-<div className="badge badge-info">Info</div>
-<div className="badge badge-success">Success</div>
-<div className="badge badge-warning">Warning</div>
-<div className="badge badge-error">Error</div>
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/setting" element={<SettingPage />} />
+
+        <Route path="/profile" element={<ProfilePage />} />
+
+      </Routes>
     </div>
   )
 }
