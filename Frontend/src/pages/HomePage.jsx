f59f9f9769766e6dfc52558d66b2526f815a8d2e
ILakshitha <ilakshitha7921@gmail.com>
2025-05-18 12:00:49 +0530

    page routes checked

---
+++ Frontend/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the main page of the application.</p>
+        <p>Here you can find various features and functionalities.</p>
+        <p>Feel free to explore!</p>
+    </div>
+  )
+}
+
+export default HomePage
