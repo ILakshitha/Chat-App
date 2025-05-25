ee45048a79bbbb36caf9023b81ce5e32d0030bbf
ILakshitha <ilakshitha7921@gmail.com>
2025-05-25 13:25:09 +0530

    change data for deployment

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -9,11 +9,14 @@ import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { server,app,io } from "./lib/socket.js";
 
+import path from "path";
+
 
 dotenv.config();
 // const app = express();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -23,6 +26,15 @@ app.use(cors({
 }));
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+
+if(process.env.NODE_ENV === "production"){
+app.use(express.static(path.join(__dirname, "../Frontend/dist")));
+
+app.get("*", (req,res)=>{
+    res.sendFile(path.join(__dirname, "../Frontend","dist","index.html"));
+} )
+}
+
 
 
 server.listen(PORT, ()=>{
